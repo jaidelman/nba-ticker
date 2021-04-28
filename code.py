8822fc31669689899bad5376a69312f7dfc3514c
jaidelman <jaidelma@uoguelph.ca>
2021-04-27 22:35:22 -0400

    Change refresh rate depending on if games are in progress or not

--- code.py
+++ code.py
@@ -104,101 +104,124 @@ matrixportal.add_text(
 
 # Replaces the score with the time and quarter and scrolls
 # the time across the board
-
-
 def displayTime(game):
     matrixportal.set_text("", 0)
     matrixportal.set_text("", 1)
     matrixportal.set_text("", 2)
     matrixportal.set_text("", 3)
+    
+    # If away team is winning use their color, if home team is winning (or game hasn't started) use home color
+    if game["awayScore"] > game["homeScore"]:
+        matrixportal.set_text_color(game["awayColors"]["primary"], 8)
+    else:
+        matrixportal.set_text_color(game["homeColors"]["primary"], 8)
 
-    matrixportal.set_text_color(game["homeColors"]["primary"], 8)
-
+    # Based on the info we have display different things
     if game['statusDesc'] is not None and game['clock'] is not None:
         matrixportal.set_text(f'{game["clock"]} {game["statusDesc"]}', 8)
     elif game['statusDesc'] is not None:
         matrixportal.set_text(game["statusDesc"], 8)
     else:
-        matrixportal.set_text(
-            f'{game["awayAbbr"]} VS. {game["homeAbbr"]} - {game["startTime"]}', 8)
+        matrixportal.set_text(f'{game["awayAbbr"]} VS. {game["homeAbbr"]} - {game["startTime"]}', 8)
     matrixportal.scroll_text(SCROLL_SPEED)
 
 # Gets the scores from our API and stores them in a data object
-
-
 def getScores():
-    print("Updating scores")
+    print("Getting scores")
     gc.collect()
     response = matrixportal.fetch()
     data = json.loads(response)
 
     return data
 
-# Sets the scores on the board
-
-
+# Sets the scores on the board, returns if the game has started or not
 def setScore(game):
-
+    
     # Away Name Shadow
     matrixportal.set_text(game["awayAbbr"], 4)
     matrixportal.set_text_color(game["awayColors"]["alt"], 4)
-
+    
     # Away Name
     matrixportal.set_text(game["awayAbbr"], 6)
     matrixportal.set_text_color(game["awayColors"]["primary"], 6)
-
+    
     # Home Name Shadow
     matrixportal.set_text(game["homeAbbr"], 5)
     matrixportal.set_text_color(game["homeColors"]["alt"], 5)
-
+    
     # Home Name
     matrixportal.set_text(game["homeAbbr"], 7)
     matrixportal.set_text_color(game["homeColors"]["primary"], 7)
-
+    
     if game["awayScore"] == 0 and game["homeScore"] == 0:
         return False
     else:
         # Away Score Shadow
         matrixportal.set_text(game["awayScore"], 0)
         matrixportal.set_text_color(game["awayColors"]["alt"], 0)
-
+        
         # Away Score
         matrixportal.set_text(game["awayScore"], 2)
         matrixportal.set_text_color(game["awayColors"]["primary"], 2)
-
+        
         # Home Score Shadow
         matrixportal.set_text(game["homeScore"], 1)
         matrixportal.set_text_color(game["homeColors"]["alt"], 1)
-
+        
         # Home Score
         matrixportal.set_text(game["homeScore"], 3)
         matrixportal.set_text_color(game["homeColors"]["primary"], 3)
-
+        
         return True
 
+# Checks if there are any ongoing games, this is to set the refresh rate (we want to refresh more often if there are)
+def areThereOngoingGames(data):
+    # Loop through all games to see if one is going on
+    for game in data['body']['payload']:
+        # If the game has a score but it's not the final score, it is ongoing so return true
+        if (game["awayScore"] != 0 and game["homeScore"] != 0) and game['statusDesc'] != "FINAL":
+            return True
+      
+    # If we get though the loop without finding a game in progress we can return false
+    return False
 
-data = None
-refresh_time = None
-index = 0
+# --- Main --- #
+
+REFRESH_RATE_DURING_GAMES = 900 # If games are going on, update scores every 15 minutes
+REFRESH_RATE_OUTSIDE_GAMES = 1800 # If there are no games on, wait half an hour before checking again
+data = None # Store the live data
+refresh_time = None # Store time of last refresh
+refresh_rate = None # Store refresh rate
+i = 0 # Increment counter
+
 while True:
-
-    if (not refresh_time) or (time.monotonic() - refresh_time) > 900:
+    
+    # If it's the first loop or it's been longer than our refresh rate, get live scores
+    if (not refresh_time) or ((time.monotonic() - refresh_time) > refresh_rate):
         data = getScores()
+        refresh_rate = REFRESH_RATE_DURING_GAMES if areThereOngoingGames(data) else REFRESH_RATE_OUTSIDE_GAMES # Update refresh rate (in case all games ended)
         refresh_time = time.monotonic()
-
+        print("Refreshing in {:.0f} minutes".format(refresh_rate/60))
+    
+    # Display our information
     if data is not None:
+        index = int(i/2) # We want to show the score/time twice before switching to the next game
         game = data['body']['payload'][index]
         gameStarted = setScore(game)
-
-        if gameStarted:
-            time.sleep(5)
+        
+        # If the games started, wait 10 seconds then show the status/time
+        if gameStarted: 
+            time.sleep(10)
             displayTime(game)
+        # If the game hasn't started, display start time twice
         else:
             displayTime(game)
             time.sleep(2)
             displayTime(game)
-
-        if index < len(data['body']['payload']) - 1:
-            index += 1
+            time.sleep(2)
+        
+        # Increment index
+        if i < len(data['body']['payload'])*2 - 1:
+            i += 1
         else:
-            index = 0
+            i = 0
